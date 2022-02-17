dbdea17abe28156231d696235ec2d3046521531b
vattvichet <vichetvattedu@gmail.com>
2022-02-17 22:17:53 +0700

    funtion

---
+++ first.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+function checkDriverAge(age) {
+
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+console.log(checkDriverAge(18));
